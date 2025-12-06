6bbcab0f9257008db9ab2ef374246687ebb085da
aryanvoid <aryanvoid505@gmail.com>
2025-12-06 22:53:48 +0530

    refactor: updated home routes with improved logic

--- Routes/home-routes.js
+++ Routes/home-routes.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const authMiddleware = require('../middleware/auth-middleware');
 
-router.get('/c', authMiddleware, (req, res) => {
+router.get('/welcome', authMiddleware, (req, res) => {
   res.json({
     message: 'Welcome to home page!',
     user: req.user
